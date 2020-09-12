11b0e0ea30c40917d4b75907df12636c69c85353
Damon German <damon.german@hyland.com>
2020-09-12 13:22:28 -0500

    made some changes

--- ISP/Program.cs
+++ ISP/Program.cs
@@ -6,15 +6,36 @@ namespace ISP
     {
         private static void Main()
         {
-            Operate(new MultiFunctionalCar());
-            // Operate(new Car());
-            // Operate(new Airplane());
+            //Operate(new MultiFunctionalCar());
+            TryOperate(new Car());
+            TryOperate(new Airplane());
         }
 
         private static void Operate(IVehicle vehicle)
         {
             vehicle.Drive();
             vehicle.Fly();
+        }
+
+        private static void TryOperate(IVehicle vehicle)
+        {
+            try
+            {
+                vehicle.Fly();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            try
+            {
+                vehicle.Drive();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
