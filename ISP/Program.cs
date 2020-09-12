4355e0c408bf3b4b4b8c934107c379aa3e36e81d
Damon German <damon.german@hyland.com>
2020-09-12 12:45:16 -0500

    made some changes

---
+++ ISP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ISP
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Operate(new MultiFunctionalCar());
+            // Operate(new Car());
+            // Operate(new Airplane());
+        }
+
+        private static void Operate(IVehicle vehicle)
+        {
+            vehicle.Drive();
+            vehicle.Fly();
+        }
+    }
+}
